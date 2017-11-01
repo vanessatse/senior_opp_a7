4ba936963ec5454e25b1d359755bda0b2da642ff
Celine Wong <Celine Wong>
2017-10-31 22:48:27 -0700

    added sources

--- js/volResults.js
+++ js/volResults.js
@@ -1,3 +1,5 @@
+/*SOURCE: https://www.w3schools.com/howto/howto_js_dropdown.asp */
+
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
 function myFunction() {
