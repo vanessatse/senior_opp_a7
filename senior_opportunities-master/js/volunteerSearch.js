fe4d6115343b460e95de4bce23085fa0efc196d0
vanessatse <32582209+vanessatse@users.noreply.github.com>
2017-11-04 16:44:44 -0700

    Add files via upload

---
+++ senior_opportunities-master/js/volunteerSearch.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+  $('#submitBtn').click(function() {
+    if (validateForm())
+      $('#vForm').submit();
+  });
+
+  $('#zip').change(function() {
+    if (!isValidZip()) {
+      $('#zipHelp').show();
+      $('#zipHelp').removeAttr("hidden");
+    }
+    else
+      $('#zipHelp').hide();
+  });
+
+  $('#dist').change(function() {
+    if (!isValidDist()) {
+      $('#distHelp').show();
+      $('#distHelp').removeAttr("hidden");
+    }
+    else
+      $('#distHelp').hide();
+  });
+
+  function validateForm() {
+    var validZip = isValidZip();
+    var validDist = isValidDist();
+
+    if (!validDist) {
+      $('#distHelp').removeAttr("hidden");
+      $('#dist').focus();
+      $('#dist').val('');
+    }
+    if (!validZip) {
+      $('#zipHelp').removeAttr("hidden");
+      $('#zip').focus();
+      $('#zip').val('');
+    }
+    return (validZip && validDist);
+  }
+
+  function isValidZip() {
+    var zip = $('#zip').val();
+    if (isNaN(Number(zip)) || zip.length < 5)
+      return false;
+    return true;
+  }
+
+  function isValidDist() {
+    var dist = Number($('#dist').val());
+    if (isNaN(dist) || dist >= 300 || dist == 0)
+      return false;
+    return true;
+  }
+});
